245cca53735a2df2486ad2a522d89c96d3dd95af
Jeremy Le François <jelefra@users.noreply.github.com>
2022-10-28 00:00:21 +0100

    Create fewer Redis clients

--- pages/api/getRedisKeys.ts
+++ pages/api/getRedisKeys.ts
@@ -5,11 +5,11 @@ const getRedisKeys = async (req: NextApiRequest, res: NextApiResponse) => {
   const { keys: keysParam } = req.query;
   if (typeof keysParam === 'string') {
     const keys = keysParam.split(',');
+    const client = createClient();
+    await client.connect();
 
     const redisKeyValuePairs = await Promise.all(
       keys.map(async (key) => {
-        const client = createClient();
-        await client.connect();
         const value = await client.get(key);
         return { key, value: JSON.parse(value) };
       })
