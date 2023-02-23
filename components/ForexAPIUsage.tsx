70863efab20a00765c66784b1a4eb3f33ba0c341
Jeremy Le François <jelefra@users.noreply.github.com>
2023-02-23 23:18:00 +0000

    Fix forex API usage projection

--- components/ForexAPIUsage.tsx
+++ components/ForexAPIUsage.tsx
@@ -6,7 +6,7 @@ const ForexAPIUsage = ({ usage }: { usage: OpenExchangeRatesUsage }) => {
   const { requests, requests_quota, days_elapsed, days_remaining } = usage;
   const currentPct = requests / requests_quota;
   // days_elapsed = 0 on the first day of the cycle
-  const projectionPct = (currentPct / (days_elapsed + 1)) * (days_remaining - 1);
+  const projectionPct = (currentPct / (days_elapsed + 1)) * (days_elapsed + days_remaining);
 
   return (
     <APIUsage
